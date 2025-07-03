c056097271bea22c79ac20d79165b461e725b66e
JanetVN1201 <58976783+JanetVN1201@users.noreply.github.com>
2025-07-03 14:08:35 +0200

    Updated files

--- Joint models workshop/Examples in R.R
+++ Joint models workshop/Examples in R.R
@@ -1,12 +1,8 @@
-### Examples CDC training - day 3 - part 1
-
 library(INLA)
 library(INLAjoint)
 library(ggplot2)
 library(boot)
-
-setwd("/home/dr/Documents/CDC24")
-load("Data/Data_day3.RData") # contains 3 datasets (pbc2 from package JM, bmt from package smcure and readmission from package frailtypack)
+library(JMbayes2)
 
 head(pbc2) # primary biliary cholangitis dataset
 pbc2$death <- ifelse(pbc2$status=="dead",1,0)
@@ -32,11 +28,9 @@ summary(M1.wei)
 M1.exp_2 <- joint(formSurv = inla.surv(years, death) ~ drug + sex,
                   dataSurv = SurvData, basRisk="exponentialsurv")
 summary(M1.exp_2)
-inla.zmarginal(inla.tmarginal(function(x) exp(x), M1.exp$marginals.fixed$`(Intercept)`))
-
 summary(M1.exp_2, hr=TRUE) # hazard ratios instead of betas
 
-# Weibull baseline risk
+# Model 2 - Weibull baseline risk
 M1.wei_2 <- joint(formSurv = inla.surv(years, death) ~ drug + sex,
                   dataSurv = SurvData, basRisk="weibullsurv")
 summary(M1.wei_2) # shape = 1 => exponential
@@ -47,7 +41,7 @@ plot(M1.wei_2)
 # => Can be formulated as AFT!
 
 
-# smooth semiparametric baseline risk (random walk order 2)
+# Model 3 - smooth semiparametric baseline risk (random walk order 2)
 M1_3 <- joint(formSurv = inla.surv(years, death) ~ drug + sex,
               dataSurv = SurvData, basRisk="rw2", NbasRisk = 30)
 summary(M1_3, hr=T)
@@ -79,13 +73,7 @@ lines(P2$time, P2$Surv_quant0.975, col=2, lty=2)
 legend("topright", c("Female", "Male"), lty=c(1,1), col=c(1,2))
 summary(SurvData) # more data om females => less uncertainty
 
-
-
-
-
-
-
-## Model 2: Competing risks
+## Model 4 - Competing risks
 SurvData2 <- pbc2[c(which(diff(as.numeric(pbc2[,which(colnames(pbc2)=="id")]))==1),
                    length(pbc2[,which(colnames(pbc2)=="id")])),c("id", "years", "death", "trans","drug", "sex")]
 head(SurvData2)
@@ -103,22 +91,110 @@ NewData2$id <- 1:4
 NewData2
 
 P2 <- predict(M2, NewData2, horizon=14, CIF=TRUE, id="id")
-# CIFs are preferred over survival curves in the context of
-# competing risks as survival curves would represent the
-# probability of having an event in a hypothetical world
-# where it is not possible to have any of the competing events.
+# CIF is preferred because it accounts for other competing events
 ggplot(P2$PredS, aes(x=time, y=CIF_quant0.5, group=id)) +
   geom_line(aes(color=id)) +
   geom_line(aes(x=time, y=CIF_quant0.025, color=id), linetype="dashed")+
   geom_line(aes(x=time, y=CIF_quant0.975, color=id), linetype="dashed")+
   facet_wrap(~Outcome+id, ncol=4)
 
+# Model 5 - Longitudinal model
+
+LongData <- pbc2[, c("id", "year", "serBilir","drug")]
+print(head(LongData, 10), row.names=F)
+# mixed effects regression model
+M1 <- joint(formLong = serBilir ~ year + drug  +  (1 + year|id),
+            dataLong = LongData, id = "id", timeVar = "year",
+            family = "lognormal")
+summary(M1)
+summary(M1, sdcor=TRUE)
+inla.priors.used(M1)
+plot(M1)
+plot(M1, priors=TRUE, sdcor=TRUE)$Covariances
+
+
+Longi <- na.omit(pbc2[, c("id", "years", "status","drug","age",
+                          "sex","year","serBilir","SGOT", "albumin", "edema",
+                          "platelets", "alkaline","spiders", "ascites")])
+Surv <- Longi[c(which(diff(as.numeric(Longi[,which(colnames(Longi)=="id")]))==1),
+                length(Longi[,which(colnames(Longi)=="id")])),-c(7:10, 12:16)]
+Surv$death <- ifelse(Surv$status=="dead",1,0) # competing event 1
+Surv$trans <- ifelse(Surv$status=="transplanted",1,0) # competing event 2
+
+
+# empirical Bayes: same frequentist properties / cheaper computational cost
+
+# Model 6 - Joint model with functions of time and current value + current slope association
+f1 <- function(x) x^2
+f2 <- function(x) x^3
+
+M4 <- joint(formSurv =  inla.surv(time = years, event = death)  ~ drug,
+            formLong = serBilir ~ (1 + year + f1(year) + f2(year))*drug +
+              (f1(year) + f2(year) |id), family = "lognormal",
+            dataLong = Longi, dataSurv = Surv, id = "id", timeVar = "year", assoc = "CV_CS",
+            basRisk = "rw2", NbasRisk=25, control=list(int.strategy="eb", safemode=F))
+summary(M4)
+plot(M4, sdcor=T)
 
 
 
 
 
-## Model 3: Multi-state model (e.g., illness-death)
+
+# Model 7 - Joint model for longitudinal and competing risk
+M6 <- joint(formSurv = list(inla.surv(time = years, event = death)  ~ sex + drug,
+                            inla.surv(time = years, event = trans) ~ edema * sex),
+            formLong = serBilir ~ year * (drug + sex) + (1+year|id),
+            dataLong = Longi, dataSurv=Surv, id = "id", timeVar = "year", family = "lognormal",
+            basRisk = c("rw1", "rw1"), assoc = c("SRE", "SRE_ind"), control=list(int.strategy="eb", safemode=F))
+summary(M6)
+
+
+
+
+
+
+# Model 8 - Bivariate joint model with competing risks
+M8 <- joint(formLong = list(serBilir ~ year * drug + sex + (1|id),
+                            platelets ~ year + f1(year) + drug + sex + (1|id)),
+            formSurv = list(inla.surv(time = years, event = death) ~ drug,
+                            inla.surv(time = years, event = trans) ~ drug),
+            dataLong = Longi, dataSurv=Surv, id = "id", corLong=TRUE, timeVar = "year",
+            family = c("lognormal", "poisson"), basRisk = c("rw1", "rw1"),
+            assoc = list(c("CV", "CV"), c("SRE", "")),
+            control=list(int.strategy="eb"))
+summary(M8)
+
+
+
+
+# Model 9 - Multivariate joint model
+Nsplines <- ns(Longi$year, knots=c(1,4))
+f1 <- function(x) predict(Nsplines, x)[,1]
+f2 <- function(x) predict(Nsplines, x)[,2]
+f3 <- function(x) predict(Nsplines, x)[,3]
+
+M16 <-joint(formSurv = list(inla.surv(time = years, event = death) ~ drug,
+                            inla.surv(time = years, event = trans) ~ drug),
+            formLong = list(serBilir ~ (1 + f1(year) + f2(year) + f3(year)) * drug +
+                              (1 + f1(year) + f2(year) + f3(year) | id),
+                            SGOT ~ (1 + f1(year) + f2(year) + f3(year)) * drug +
+                              (1 + f1(year) + f2(year) + f3(year) | id),
+                            albumin ~ (1 + year) * drug + (1 + year | id),
+                            platelets ~ (1 + f1(year) + f2(year) + f3(year)) * drug +
+                              (1 + f1(year) + f2(year) + f3(year) | id),
+                            spiders ~ (1 + year) * drug + (1 + year | id)),
+            dataLong = Longi, dataSurv = Surv, id = "id", timeVar = "year", corLong = F,
+            family = c("lognormal", "lognormal", "gaussian", "poisson", "binomial"),
+            basRisk = c("rw2", "rw1"), NbasRisk = 15, assoc = list(c("CV_CS", "CV"),
+                                                                   c("CV", ""), c("CV", "CV"), c("CV", "CV"), c("CV", "")),
+            control=list(int.strategy="eb"))
+summary(M16)
+
+
+#Extra examples
+
+## Multi-state model (e.g., illness-death)
 data(SurvMS) # package INLAjoint
 E12 <- inla.surv(time = SurvMS[[1]]$Tstop, event = SurvMS[[1]]$status) # transition 1->2
 E13 <- inla.surv(time = SurvMS[[2]]$Tstop, event = SurvMS[[2]]$status) # transition 1->3
@@ -130,11 +206,7 @@ M3 <- joint(formSurv=list(E12 ~ X, E13 ~ X, E23 ~ X),
             dataSurv = SurvMS)
 summary(M3)
 
-
-
-
-
-## Model 4: Mixture cure model
+## Mixture cure model
 # library(smcure)
 # ?bmt
 #data(bmt) # package smcure
@@ -161,7 +233,7 @@ summary(M4, hr=T)
 
 
 
-## Model 5: shared frailty model for recurrent events
+## Shared frailty model for recurrent events
 # data(readmission) # package frailtypack
 # ?readmission
 M5 <- joint(formSurv=inla.surv(t.stop, event) ~ sex + (1|id), id="id", dataSurv = readmission)
@@ -176,7 +248,7 @@ summary(M5, hr=T)
 
 
 
-## Model 6: shared frailty model for recurrent and terminal event
+## Shared frailty model for recurrent and terminal event
 terminalData <- readmission[readmission$event==0,]
 M6 <- joint(formSurv=list(inla.surv(t.stop, event) ~ sex + (1|id), # risk of rehospitalization
                            inla.surv(t.stop, death) ~ sex), id="id", # risk of death
@@ -185,6 +257,22 @@ M6 <- joint(formSurv=list(inla.surv(t.stop, event) ~ sex + (1|id), # risk of reh
 summary(M6)
 
 
+
+
+# Joint model for longitudinal and multi-state
+data(SurvMS) # package INLAjoint
+data(LongMS) # package INLAjoint
+E12 <- inla.surv(time = SurvMS[[1]]$Tstop, event = SurvMS[[1]]$status) # transition 1->2
+E13 <- inla.surv(time = SurvMS[[2]]$Tstop, event = SurvMS[[2]]$status) # transition 1->3
+E23 <- inla.surv(time = SurvMS[[3]]$Tstop, truncation=SurvMS[[3]]$Tstart,
+                 event =SurvMS[[3]]$status) # transition 2->3
+M7 <- joint(formSurv=list(E12 ~ X, E13 ~ X, E23 ~ X),
+            formLong=list(y ~ time + X + (1+time|id)),
+            basRisk = c("rw2", "rw1", "exponentialsurv"), timeVar = "time",
+            assoc = list(c("CV", "CV", "CV")), id="id",
+            dataSurv = SurvMS, dataLong = LongMS,
+            cutpoints=seq(0, max(SurvMS[[3]]$Tstop), len=15))
+summary(M7)
 
 
 
